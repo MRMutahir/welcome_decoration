64c73dcadce75f873665c5df8a08ae8755548370
MRMutahir <mutahirkareem820@gmail.com>
2024-02-19 19:16:37 +0500

    Gallery start

---
+++ app/Gallery/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+import Services from "../components/Services";
+
+const page = () => {
+  return (
+    <div className="flex">
+      <SideBar />
+      <Services />
+    </div>
+  );
+};
+
+export default page;
