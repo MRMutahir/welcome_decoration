04117461dbc0c5709fff7521c3d3955554d3a92f
MRMutahir <mutahirkareem820@gmail.com>
2024-02-19 18:20:18 +0500

    video set

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -2,25 +2,23 @@ import React from "react";
 
 const Services = () => {
   return (
-    <div className="rightBar  overflow-auto w-screen h-screen">
-      <div className="h-screen relative">
+    <div className="rightBar overflow-auto w-screen h-screen">
+      <div className="h-screen">
         <video
           // src="/video/video.mp4"
           // src="https://firebasestorage.googleapis.com/v0/b/video-sharing-app-f1f97.appspot.com/o/video%2Fvideo.mp4?alt=media&token=d634aedf-3222-46cc-b05b-9c9b8e070f53"
-          className="h-screen object-cover"
+          className="h-screen object-cover fixed"
           src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
           autoPlay
           loop
           muted
-          // style={{ maxWidth: "100vw", height: "auto" }}
+          // style={{ maxWidth: "100vw", height: "auto" }
         ></video>
-        <div className="absolute top-0 flex justify-center items-center flex-col mx-auto">
-          <div>
-            {" "}
+        <div className="flex justify-center items-center flex-col relative top-0">
+          <div className="">
             <h1 className="text-7xl font-semibold font-mono">Our Services</h1>
           </div>
-          <div>
-            {" "}
+          <div className="">
             <div className="flex justify-between items-center gap-6 w-full ">
               <div className="card w-96 bg-base-100 shadow-xl">
                 <figure>
