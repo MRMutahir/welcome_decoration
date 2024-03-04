057c5da0e4997d4229ef8735587505eacd23a4aa
MuhammadEshareeb <eshareebm@gmail.com>
2024-03-04 00:06:52 -0800

    work  in  Gallery

--- app/Gallery/page.jsx
+++ app/Gallery/page.jsx
@@ -1,16 +1,14 @@
 // import React from "react";
 // import SideBar from "../components/SideBar";
-// import Gallery from "../components/Gallery";
-// import DaisyNavbar from "../components/DaisyNavbar";
+import Gallery from "../components/Gallery";
+import DaisyNavbar from "../components/DaisyNavbar";
 
 const page = () => {
-  return;
-
-  <>
-    <h1>SALAM</h1>
-    {/* <DaisyNavbar /> */}
-    {/* <Gallery /> */}
-  </>;
+  return (
+    <>
+      <DaisyNavbar /> <Gallery />
+    </>
+  );
 };
 
 export default page;
